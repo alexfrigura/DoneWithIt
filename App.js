cb10a4145792cfeb7ce242295778aa9371c3cf8c
alexfrigura <alexfrigura15@gmail.com>
2021-05-12 21:03:12 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,4 @@
-import React, {useState} from 'react';
-import { SwitchComponent, Text, TextInput, View, Switch, InputAccessoryView } from 'react-native';
-import Card from './app/components/Card';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import Screen from "./app/components/Screen";
-import ListItem from "./app/components/ListItem";
-import Icon from "./app/components/Icon";
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
+import React from 'react';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 
 
