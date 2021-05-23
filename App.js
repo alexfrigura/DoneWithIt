008536717643f2e51b2e6989037cdbfd5ff55d0d
alexfrigura <83580469+alexfrigura@users.noreply.github.com>
2021-05-23 12:31:04 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Screen from './app/components/Screen';
 
@@ -57,21 +58,28 @@ const StackNavigator = () => (
 const Account = () => <Screen><Text>Account</Text></Screen>
 
 const Tab = createBottomTabNavigator();
-const TabNavigator = ()=> (
+const TabNavigator = ()  => (
   <Tab.Navigator
     tabBarOptions={{
       activeBackgroundColor: 'tomato',
       activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
     }}>
-    <Tab.Screen name="Feed" component={Tweets}/>
+    <Tab.Screen 
+    name="Feed" 
+    component={Tweets}
+    options={{
+      tapBarIcon: ({ size, color }) => (<MaterialCommunityIcons name="home" size={size} color={color}/>),
+    }}/>
     <Tab.Screen name="Account" component={Account}/>
   </Tab.Navigator>
-)
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
